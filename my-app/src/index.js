dc07f49015ef884e15e5c7c225b9748ba3ea28ab
cruzjoshua <cruzj4@hawaii.edu>
2019-02-25 17:02:52 -1000

    Task 4
    Final

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Grid, Header, Icon, Image, Input, List, Menu , Label } from 'semantic-ui-react';
+import { Container, Grid, Header, Icon, Image, Input, Label, List, Menu } from 'semantic-ui-react';
 
 class TopMenu extends React.Component {
   render() {
@@ -32,13 +32,15 @@ class MiddlePicture extends React.Component {
 class MiddleContainer extends React.Component {
   render() {
     return (
-        <Container>
-          <br/>
-          <Header as="h3">FEATURED EVENTS</Header><Header as="h5">all events</Header>
-          <br/>
-          <br/>
-          <br/>
-        </Container>
+        <div className='middle'>
+          <Container>
+            <br/>
+            <Header as="h3">FEATURED EVENTS</Header><Header as="h5">all events</Header>
+            <br/>
+            <br/>
+            <br/>
+          </Container>
+        </div>
     )
   }
 }
@@ -46,72 +48,74 @@ class MiddleContainer extends React.Component {
 class MiddlePictureRow extends React.Component {
   render() {
     return (
-        <Container>
-          <br/>
-          <br/>
-          <Grid columns={4}>
-            <Grid.Column>
-              <Image src='http://jointherepublik.com/wp-content/uploads/2018/12/Ninja_Sex_Party_2019-1-615x615.jpg'
-                     size='medium'/>
-              <List>
-                <List.Item>
-                  NINJA SEX PARTY: A GREAT LEI
-                </List.Item>
-                <List.Item>
-                  February 20
-                </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column>
-              <Image
-                  src='http://jointherepublik.com/wp-content/uploads/2020/01/Atmosphere_Oahu_ig_1600x1600-615x615.jpg'
-                  size='medium'/>
-              <List>
-                <List.Item>
-                  ATMOSPHERE
-                </List.Item>
-                <List.Item>
-                  April 6
-                </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column>
-              <Image
-                  src='http://jointherepublik.com/wp-content/uploads/2019/02/Screenshot-2019-02-11-20.02.24-615x615.png'
-                  size='medium'/>
-              <List>
-                <List.Item>
-                  DESENDENTS
-                </List.Item>
-                <List.Item>
-                  April 19
-                </List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column>
-              <Image
-                  src='http://jointherepublik.com/wp-content/uploads/2019/01/SHALLOUTour-Poster-Square-1-1-615x615.jpg'
-                  size='medium'/>
-              <List>
-                <List.Item>
-                  SLOW MAGIC & SHALLOU: INTO THE WILD TOUR
-                </List.Item>
-                <List.Item>
-                  May 4
-                </List.Item>
-              </List>
-            </Grid.Column>
-          </Grid>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-        </Container>
+        <div className='middle'>
+          <Container>
+            <br/>
+            <br/>
+            <Grid columns={4}>
+              <Grid.Column>
+                <Image src='http://jointherepublik.com/wp-content/uploads/2018/12/Ninja_Sex_Party_2019-1-615x615.jpg'
+                       size='medium'/>
+                <List>
+                  <List.Item>
+                    NINJA SEX PARTY: A GREAT LEI
+                  </List.Item>
+                  <List.Item>
+                    February 20
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                    src='http://jointherepublik.com/wp-content/uploads/2020/01/Atmosphere_Oahu_ig_1600x1600-615x615.jpg'
+                    size='medium'/>
+                <List>
+                  <List.Item>
+                    ATMOSPHERE
+                  </List.Item>
+                  <List.Item>
+                    April 6
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                    src='http://jointherepublik.com/wp-content/uploads/2019/02/Screenshot-2019-02-11-20.02.24-615x615.png'
+                    size='medium'/>
+                <List>
+                  <List.Item>
+                    DESENDENTS
+                  </List.Item>
+                  <List.Item>
+                    April 19
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                    src='http://jointherepublik.com/wp-content/uploads/2019/01/SHALLOUTour-Poster-Square-1-1-615x615.jpg'
+                    size='medium'/>
+                <List>
+                  <List.Item>
+                    SLOW MAGIC & SHALLOU: INTO THE WILD TOUR
+                  </List.Item>
+                  <List.Item>
+                    May 4
+                  </List.Item>
+                </List>
+              </Grid.Column>
+            </Grid>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+          </Container>
+        </div>
     )
   }
 }
